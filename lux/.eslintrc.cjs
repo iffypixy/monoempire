5c388102932aeef49e60041276551ce16720a96b
Ansat Euler <iffypixy@gmail.com>
2023-07-07 00:15:52 +0600

    fix(lux): fix no-var-requires rule

--- lux/.eslintrc.cjs
+++ lux/.eslintrc.cjs
@@ -17,10 +17,12 @@ module.exports = {
     rules: {
         "@typescript-eslint/no-non-null-assertion": "off",
     },
-    overrides: {
-        files: "*.cjs",
-        rules: {
-            "typescript-eslint/no-var-requires": "off",
+    overrides: [
+        {
+            files: "*.cjs",
+            rules: {
+                "@typescript-eslint/no-var-requires": "off",
+            },
         },
-    },
+    ],
 };
