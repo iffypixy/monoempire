4fd66d64e7f593cf1bffba86937e58297db545d0
Ansat Euler <iffypixy@gmail.com>
2023-07-07 01:29:19 +0600

    feat(lux): add i18next-scanner config

--- lux/i18next-scanner.config.cjs
+++ lux/i18next-scanner.config.cjs
@@ -1,22 +1,38 @@
+/*
+@usage:
+
+- const {t} = useTranslation("ns");
+- const {t} = useTranslation(["ns1", "ns2", "ns3"]);
+
+t("key") -> ns1:key
+t("ns2:key") -> ns2:key
+t("ns3:key") -> ns3:key
+*/
+
 const fs = require("fs");
 
-const defaultNs = "common";
-const defaultValue = "%!% :NOT TRANSLATED: %!%";
-const languages = ["en", "de"];
 const functions = ["t"];
+const defaultValue = "%!% :NOT TRANSLATED: %!%";
+
+// @dependency: i18n/config.ts
+const defaultNs = "common";
+const nss = ["common", "home"];
+const languages = ["en", "de", "fr", "es", "ru", "cn", "jp", "il", "ae"];
 
 module.exports = {
     input: "src/**/*.{js,jsx,ts,tsx}",
-    output: "public/locales",
+    output: "./",
     options: {
         debug: false,
         lngs: languages,
+        ns: nss,
         defaultLng: "en",
         defaultValue,
+        defaultNs,
         attr: false,
         resource: {
-            loadPath: "/public/locales/{{lng}}/{{ns}}.json",
-            savePath: "/public/locales/{{lng}}/{{ns}}.json",
+            loadPath: "public/locales/{{lng}}/{{ns}}.json",
+            savePath: "public/locales/{{lng}}/{{ns}}.json",
             jsonIndent: 4,
         },
         keySeparator: ".",
@@ -36,7 +52,7 @@ module.exports = {
         const match = content.match(/useTranslation\(.+\)/);
         const withNs = !!match;
 
-        if (withNs) ns = match[0].split(/("|")/)[2];
+        if (withNs) ns = match[0].split(/('|")/)[2];
 
         let count = 0;
 
@@ -49,6 +65,23 @@ module.exports = {
                     ns: ns || defaultNs,
                     nsSeparator: ":",
                     keySeparator: ".",
+                });
+
+                count++;
+            },
+        );
+
+        parser.parseTransFromString(
+            content,
+            {component: "Trans", i18nKey: "i18nKey"},
+            (key, options) => {
+                const split = key.split(":");
+
+                parser.set(split[1], {
+                    ...options,
+                    ns: split[0],
+                    nsSeparator: false,
+                    keySeparator: false,
                 });
 
                 count++;
