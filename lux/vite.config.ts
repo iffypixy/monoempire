b97547a0d56d8a7aa502e09fb7cf325e633b5bf0
Ansat Euler <iffypixy@gmail.com>
2023-07-08 01:50:59 +0600

    chore(lux): init svgr

--- lux/vite.config.ts
+++ lux/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { checker } from "vite-plugin-checker";
+import {checker} from "vite-plugin-checker";
 import stylelint from "vite-plugin-stylelint";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
@@ -18,5 +19,6 @@ export default defineConfig({
             lintInWorker: true,
             lintOnStart: true,
         }),
+        svgr(),
     ],
 });
