66bcc198d825398ebbfd137cabe90a09e77caf27
Ansat Euler <iffypixy@gmail.com>
2023-07-07 22:20:17 +0600

    chore(lux): add language type

--- lux/src/shared/lib/i18n/config.ts
+++ lux/src/shared/lib/i18n/config.ts
@@ -3,7 +3,28 @@ import {initReactI18next} from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-export const languages = ["en", "de", "fr", "es", "ru", "cn", "jp", "il", "ae"];
+export type Language =
+    | "en"
+    | "de"
+    | "fr"
+    | "es"
+    | "ru"
+    | "cn"
+    | "jp"
+    | "il"
+    | "ae";
+
+export const languages: Language[] = [
+    "en",
+    "de",
+    "fr",
+    "es",
+    "ru",
+    "cn",
+    "jp",
+    "il",
+    "ae",
+];
 
 i18n.use(Backend)
     .use(LanguageDetector)
