f99224b8e04b95ff55ccf8350d7bd077b9858419
Ansat Euler <iffypixy@gmail.com>
2023-07-17 23:48:33 +0600

    chore(aeternitas): update env types

--- aeternitas/types/env.d.ts
+++ aeternitas/types/env.d.ts
@@ -2,10 +2,16 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             NODE_ENV: string;
+            SESSION_SECRET: string;
+            REDIS_HOST: string;
+            REDIS_PORT: string;
             GOOGLE_CLIENT_ID: string;
             GOOGLE_CLIENT_SECRET: string;
             GOOGLE_REDIRECT_URI: string;
-            SESSION_SECRET: string;
+            GOOGLE_USER_INFO: string;
+            GOOGLE_AUTHORIZATION: string;
+            GOOGLE_TOKEN: string;
+            GOOGLE_SCOPE: string;
             CLIENT_ORIGIN: string;
             CLIENT_GOOGLE_REGISTRATION: string;
         }
