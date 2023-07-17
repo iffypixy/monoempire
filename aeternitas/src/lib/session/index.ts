f54fe345ad449fa62200ed30bb2aa33786e237dc
Ansat Euler <iffypixy@gmail.com>
2023-07-17 23:25:04 +0600

    feat(aeternitas): add session

---
+++ aeternitas/src/lib/session/index.ts
@@ -0,0 +1,22 @@
+import s from "express-session";
+import Store from "connect-redis";
+
+import {redis} from "@lib/redis";
+import {config} from "@lib/config";
+
+const month = 2629800000;
+
+const store = new Store({client: redis});
+
+export const session = () =>
+    s({
+        secret: config.session.secret,
+        resave: false,
+        saveUninitialized: false,
+        rolling: true,
+        store,
+        cookie: {
+            maxAge: month,
+            httpOnly: true,
+        },
+    });
