e975ef6d8e76b2f5ea936fc062667e249c18e8bc
Ansat Euler <iffypixy@gmail.com>
2023-07-21 18:31:55 +0600

    chore(aeternitas): remove unnecessary typings

--- aeternitas/src/lib/open/user.ts
+++ aeternitas/src/lib/open/user.ts
@@ -1,12 +1,6 @@
 import {User} from "@prisma/client";
 
-export interface UserPublic {
-    id: string;
-    username: string;
-    avatar: string;
-}
-
-export const user = (u: User): UserPublic => ({
+export const user = (u: User) => ({
     id: u.id,
     username: u.username,
     avatar: u.avatar,
