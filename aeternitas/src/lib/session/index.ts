f123d40f186ac256d7125a647eba0d77e5fd01c6
Ansat Euler <iffypixy@gmail.com>
2023-07-24 14:05:44 +0600

    chore(aeternitas): modify session middleware

--- aeternitas/src/lib/session/index.ts
+++ aeternitas/src/lib/session/index.ts
@@ -6,17 +6,16 @@ import {config} from "@lib/config";
 
 const month = 2629800000;
 
-const store = new Store({client: redis});
+const store = new Store({client: redis.client});
 
-export const session = () =>
-    s({
-        secret: config.session.SECRET,
-        resave: false,
-        saveUninitialized: false,
-        rolling: true,
-        store,
-        cookie: {
-            maxAge: month,
-            httpOnly: true,
-        },
-    });
+export const session = s({
+    secret: config.session.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    store,
+    cookie: {
+        maxAge: month,
+        httpOnly: true,
+    },
+});
