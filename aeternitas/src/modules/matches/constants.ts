efb252ef36018aea6908b350ce2980f2809bd12c
Ansat Euler <iffypixy@gmail.com>
2023-07-28 23:54:23 +0600

    chore(aeternitas): add matches constants

---
+++ aeternitas/src/modules/matches/constants.ts
@@ -0,0 +1,9 @@
+const redis = {
+    PUBLIC_QUEUE: "public-queue",
+    MATCH: "match",
+};
+
+const MAX_PLAYERS = 10;
+const MIN_PLAYERS = 2;
+
+export const constants = {redis, MAX_PLAYERS, MIN_PLAYERS};
