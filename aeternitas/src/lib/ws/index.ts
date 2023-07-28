26b2eafb5503da96713f44875e241089a2466dec
Ansat Euler <iffypixy@gmail.com>
2023-07-28 23:51:55 +0600

    chore(aeternitas): update ws exports

--- aeternitas/src/lib/ws/index.ts
+++ aeternitas/src/lib/ws/index.ts
@@ -1,4 +1,6 @@
 import {createGateway, setup} from "./setup";
 import {service} from "./service";
+import {createEvents} from "./events";
+import {validate} from "./validation";
 
-export const ws = {createGateway, setup, service};
+export const ws = {service, createGateway, setup, validate, createEvents};
