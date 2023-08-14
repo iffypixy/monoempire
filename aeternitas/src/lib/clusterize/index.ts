57d9aa3f540527e8ec32af7a87474745c5748cd5
Ansat Euler <iffypixy@gmail.com>
2023-08-14 19:33:05 +0600

    feat(aeternitas): add clusterize lib

---
+++ aeternitas/src/lib/clusterize/index.ts
@@ -0,0 +1,20 @@
+import cluster from "cluster";
+import os from "os";
+
+export const clusterize = (cb: () => void) => {
+    if (cluster.isPrimary) {
+        const cpus = os.cpus().length;
+
+        for (let i = 0; i < cpus; i++) {
+            cluster.fork();
+        }
+
+        cluster.on("exit", (worker) => {
+            console.log(`Worker ${worker.process.pid} died. Restarting...`);
+
+            cluster.fork();
+        });
+    } else {
+        cb();
+    }
+};
