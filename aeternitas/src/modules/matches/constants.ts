5f7dccfbee900face106323d2951b5519ca149c3
Ansat Euler <iffypixy@gmail.com>
2023-08-16 00:33:07 +0600

    chore: update constants

--- aeternitas/src/modules/matches/constants.ts
+++ aeternitas/src/modules/matches/constants.ts
@@ -1,9 +1,19 @@
 const redis = {
-    PUBLIC_QUEUE: "public-queue",
+    matchmaking: {
+        PUBLIC_QUEUE: "public-matchmaking-queue",
+        COMPETITIVE_QUEUE: "competitive-matchmaking-queue",
+    },
     MATCH: "match",
 };
 
 const MAX_PLAYERS = 10;
 const MIN_PLAYERS = 2;
 
-export const constants = {redis, MAX_PLAYERS, MIN_PLAYERS};
+const MATCHMAKING_INTERVAL = 3000;
+
+export const constants = {
+    redis,
+    MAX_PLAYERS,
+    MIN_PLAYERS,
+    MATCHMAKING_INTERVAL,
+};
