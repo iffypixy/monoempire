d683ab6f52df4a6b2c9983405be31efcd9265253
Ansat Euler <iffypixy@gmail.com>
2023-08-16 00:36:26 +0600

    chore: remove unnecessary types

--- aeternitas/src/modules/matches/types.ts
+++ aeternitas/src/modules/matches/types.ts
@@ -1,45 +1,6 @@
-import {Session} from "express-session";
-
 import {CardName} from "./lib/pile";
-import {User} from "@prisma/client";
 
 export interface Card {
     id: string;
     name: CardName;
 }
-
-export type MatchType = "waiting-for-action";
-
-export interface PublicMatch {
-    id: string;
-    players: PublicMatchPlayer[];
-    spectators: User[];
-    turn: number;
-    state: {
-        type: MatchType;
-        start: number;
-        payload?: any;
-    };
-    context: {
-        reversed: boolean;
-        noped: boolean;
-        attacks: number;
-    };
-    piles: {
-        draw: Card[];
-        discard: Card[];
-    };
-}
-
-export interface PublicMatchPlayer {
-    id: string;
-    username: string;
-    hand: Card[];
-    marked: string[];
-    lost: boolean;
-}
-
-export type PublicQueue = {
-    id: Session["id"];
-    username: string;
-}[];
