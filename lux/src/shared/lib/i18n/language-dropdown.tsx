202fde2853aee2ed4f5d8afde6703938625b57db
Ansat Euler <iffypixy@gmail.com>
2023-08-24 15:59:24 +0600

    refactor: clean code

--- lux/src/shared/lib/i18n/language-dropdown.tsx
+++ lux/src/shared/lib/i18n/language-dropdown.tsx
@@ -1,14 +1,13 @@
+import {useState} from "react";
 import {useTranslation} from "react-i18next";
-import {LanguageIcon} from "@heroicons/react/24/outline";
+import {HiLanguage} from "react-icons/hi2";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import {cva} from "class-variance-authority";
-import {useState} from "react";
 
-import {Flag} from "@shared/ui/icons";
+import {Icon} from "@shared/lib/types";
+import {Icons} from "@shared/ui/icons";
 
 import {languages, Language} from "./config";
-
-type Icon = React.FC<React.SVGProps<SVGSVGElement>>;
 
 const context: Record<
     Language,
@@ -19,45 +18,45 @@ const context: Record<
 > = {
     en: {
         name: "English",
-        icon: Flag.UK,
+        icon: Icons.Flags.UK,
     },
     ae: {
         name: "العربية",
-        icon: Flag.UAE,
+        icon: Icons.Flags.UAE,
     },
     cn: {
         name: "中文",
-        icon: Flag.China,
+        icon: Icons.Flags.China,
     },
     de: {
         name: "Deutsch",
-        icon: Flag.Germany,
+        icon: Icons.Flags.Germany,
     },
     es: {
         name: "Español",
-        icon: Flag.Spain,
+        icon: Icons.Flags.Spain,
     },
     fr: {
         name: "Français",
-        icon: Flag.France,
+        icon: Icons.Flags.France,
     },
     il: {
         name: "עברית",
-        icon: Flag.Israel,
+        icon: Icons.Flags.Israel,
     },
     jp: {
         name: "日本語",
-        icon: Flag.Japan,
+        icon: Icons.Flags.Japan,
     },
     ru: {
         name: "Русский",
-        icon: Flag.Russia,
+        icon: Icons.Flags.Russia,
     },
 };
 
 const styles = {
     item: cva(
-        "flex p-3 bg-paper-primary rounded-lg items-center border-2 hover:border-accent transition-colors duration-300",
+        "flex p-3 bg-paper-primary rounded-lg items-center border-4 hover:border-accent transition-colors duration-300",
         {
             variants: {
                 isSelected: {
@@ -70,7 +69,7 @@ const styles = {
         },
     ),
     button: cva(
-        "w-14 text-primary rounded-lg p-2 hover:shadow-even-lg hover:shadow-primary hover:bg-primary hover:text-primary-contrast transition duration-500",
+        "w-14 h-14 text-primary rounded-lg p-2 hover:shadow-even-lg hover:shadow-primary hover:bg-primary hover:text-primary-contrast transition duration-500",
         {
             variants: {
                 isActive: {
@@ -99,9 +98,7 @@ export const LanguageDropdown: React.FC = () => {
         >
             <DropdownMenu.Trigger asChild className="outline-none">
                 <button>
-                    <LanguageIcon
-                        className={styles.button({isActive: isOpen})}
-                    />
+                    <HiLanguage className={styles.button({isActive: isOpen})} />
                 </button>
             </DropdownMenu.Trigger>
 
@@ -136,9 +133,9 @@ export const LanguageDropdown: React.FC = () => {
                                                     language === i18n.language,
                                             })}
                                         >
-                                            <Icon className="w-5 text-primary mr-7" />
+                                            <Icon className="w-5 text-primary mr-5" />
 
-                                            <p className="font-semibold text-base text-paper-contrast">
+                                            <p className="lowercase font-semibold text-base text-paper-contrast">
                                                 {name}
                                             </p>
                                         </div>
