1e0b067c9d0e638d249fced3421007b8d47572fd
Ansat Euler <iffypixy@gmail.com>
2023-08-24 15:58:49 +0600

    refactor: move initialization logic

--- lux/src/shared/lib/i18n/config.ts
+++ lux/src/shared/lib/i18n/config.ts
@@ -1,8 +1,3 @@
-import i18n from "i18next";
-import {initReactI18next} from "react-i18next";
-import Backend from "i18next-http-backend";
-import LanguageDetector from "i18next-browser-languagedetector";
-
 export type Language =
     | "en"
     | "de"
@@ -25,24 +20,3 @@ export const languages: Language[] = [
     "il",
     "ae",
 ];
-
-i18n.use(Backend)
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: "en",
-        debug: import.meta.env.DEV,
-        cleanCode: true,
-        interpolation: {
-            escapeValue: false,
-        },
-        backend: {
-            loadPath: "/locales/{{lng}}/{{ns}}.json",
-        },
-        ns: ["home", "common"],
-
-        detection: {
-            order: ["localStorage"],
-            caches: ["localStorage"],
-        },
-    });
