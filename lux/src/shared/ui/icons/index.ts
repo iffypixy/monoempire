abce10d4c7e2439a5c7fc562debe47f454d1b453
Ansat Euler <iffypixy@gmail.com>
2023-08-24 15:22:39 +0600

    feat: add theming icons

--- lux/src/shared/ui/icons/index.ts
+++ lux/src/shared/ui/icons/index.ts
@@ -1,1 +1,7 @@
-export * as Flag from "./flags";
+import * as Flags from "./flags";
+import {ReactComponent as CloudyMoon} from "./cloudy-moon.svg";
+import {ReactComponent as BatMoon} from "./bat-moon.svg";
+import {ReactComponent as Sun} from "./sun.svg";
+import {ReactComponent as BloodKnife} from "./blood-knife.svg";
+
+export const Icons = {Flags, CloudyMoon, BatMoon, Sun, BloodKnife};
