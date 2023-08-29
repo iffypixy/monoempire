0b76ebb5a25cf53685620dc205aa8cbf57e3316c
Ansat Euler <iffypixy@gmail.com>
2023-08-29 23:44:33 +0600

    chore: add another namespace

--- lux/src/shared/lib/i18n/init.ts
+++ lux/src/shared/lib/i18n/init.ts
@@ -3,6 +3,8 @@ import {initReactI18next} from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+const namespaces = ["common", "home", "sign-up", "sign-in"];
+
 i18n.use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
@@ -16,7 +18,7 @@ i18n.use(Backend)
         backend: {
             loadPath: "/locales/{{lng}}/{{ns}}.json",
         },
-        ns: ["home", "common"],
+        ns: namespaces,
 
         detection: {
             order: ["localStorage"],
