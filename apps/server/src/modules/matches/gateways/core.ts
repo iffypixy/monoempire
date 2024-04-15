8c1747a5424c7b7010c00d647cb80a2d9b04e8df
Ansat Euler <iffypixy@gmail.com>
2024-04-15 14:01:08 +0200

    chore: removed comments

--- apps/server/src/modules/matches/gateways/core.ts
+++ apps/server/src/modules/matches/gateways/core.ts
@@ -1,51 +1,6 @@
-// import {ws} from "@lib/ws";
-// import {redis} from "@lib/redis";
-// import {validation} from "@lib/validation";
-
-// import * as dtos from "../dtos";
-// import {constants} from "../constants";
-// import {IMatch} from "../lib/match";
-
-// const events = ws.events("match", {
-//   client: {},
-//   server: {
-//     PLAY_CARD: "play-card",
-//     PLAY_NOPE: "play-nope",
-//     JOIN: "join",
-//     RESIGN: "resign",
-//   },
-// });
-
-// export const gateway = ws.gateway((io) => {
-//   io.on("connection", (socket) => {
-//     socket.on(
-//       events.server.PLAY_CARD,
-//       ws.handler<dtos.PlayCard>(
-//         [validation.check.ws(dtos.PlayCard)],
-//         async (payload, acknowledge) => {
-//           const match = await redis.service.get<IMatch>(
-//             `${constants.redis.MATCH}:${payload.matchId}`
-//           );
-
-//           if (!match) return acknowledge(false, {msg: "No match found"});
-
-//           const isPlayer = match.players.some(
-//             (player) => player.id === socket.request.session.userId
-//           );
-
-//           if (!isPlayer)
-//             return acknowledge(false, {
-//               msg: "You are not a player of the match",
-//             });
-//         }
-//       )
-//     );
-//   });
-// });
-
 import {WebSocketGateway, OnGatewayInit} from "@nestjs/websockets";
 
 @WebSocketGateway()
-export class CoreMatchesGateway implements OnGatewayInit {
+export class MatchesGateway implements OnGatewayInit {
     afterInit() {}
 }
