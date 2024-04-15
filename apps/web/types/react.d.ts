c06177a7cd559b75d56ccbc60fd0db3af31278bf
Ansat Euler <iffypixy@gmail.com>
2024-04-15 15:06:16 +0200

    fix: adjusted comment to standard

--- apps/web/types/react.d.ts
+++ apps/web/types/react.d.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
 declare module "react" {
-    // @note: WC = with children
+    /** @note WC = with children */
     type FCWC = React.FC<{children: React.ReactNode}>;
 }
