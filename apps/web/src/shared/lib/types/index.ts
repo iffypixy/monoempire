63901fe07f0c012e887779458d6af1039da939e8
Ansat Euler <iffypixy@gmail.com>
2024-04-17 14:54:54 +0200

    refactor: adjusted icon type

--- apps/web/src/shared/lib/types/index.ts
+++ apps/web/src/shared/lib/types/index.ts
@@ -1,5 +1,3 @@
-import {IconType} from "react-icons";
-
-export type Icon = React.FC<React.ComponentProps<"svg">> | IconType;
+export type Icon = React.FC<React.ComponentProps<"svg">>;
 export type Nullable<T> = T | null;
 export type Callback = (...args: any[]) => void;
