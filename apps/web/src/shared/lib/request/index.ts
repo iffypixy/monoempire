6f6952208da55c5813d62837ee440d3bfacb557f
Ansat Euler <iffypixy@gmail.com>
2024-04-17 14:52:34 +0200

    feat: implemented dto interface

--- apps/web/src/shared/lib/request/index.ts
+++ apps/web/src/shared/lib/request/index.ts
@@ -3,3 +3,8 @@ import axios from "axios";
 export const request = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
 });
+
+export interface Dto<Req, Res> {
+    req: Req;
+    res: Res;
+}
