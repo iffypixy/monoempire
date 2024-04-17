cb9e0c65d02493494c662bf39cc4e116b55504e4
Ansat Euler <iffypixy@gmail.com>
2024-04-17 14:53:59 +0200

    refactor: adjusted routing to zustand

--- apps/web/src/shared/lib/routing/index.tsx
+++ apps/web/src/shared/lib/routing/index.tsx
@@ -1,12 +1,11 @@
-import {useSelector} from "react-redux";
 import {Redirect, Route, RouteProps} from "wouter";
 
-import {authModel} from "@features/auth";
+import {useAuthStore} from "@features/auth";
 
 type PublicOnlyRouteProps = RouteProps;
 
 export const PublicOnlyRoute: React.FC<PublicOnlyRouteProps> = (props) => {
-    const isAuthenticated = useSelector(authModel.selectors.isAuthenticated);
+    const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
 
     if (isAuthenticated) return <Redirect to="/" />;
 
@@ -16,7 +15,7 @@ export const PublicOnlyRoute: React.FC<PublicOnlyRouteProps> = (props) => {
 type PrivateRouteProps = RouteProps;
 
 export const PrivateRoute: React.FC<PrivateRouteProps> = (props) => {
-    const isAuthenticated = useSelector(authModel.selectors.isAuthenticated);
+    const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
 
     if (isAuthenticated) return <Route {...props} />;
 
