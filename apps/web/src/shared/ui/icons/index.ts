c60bd94f7d2ff295432de8e9dd193392a0c8eb94
Ansat Euler <iffypixy@gmail.com>
2024-04-19 15:31:46 +0200

    feat: added bunch of icons

--- apps/web/src/shared/ui/icons/index.ts
+++ apps/web/src/shared/ui/icons/index.ts
@@ -1,8 +1,27 @@
 import * as Flags from "./flags";
-import {ReactComponent as CloudyMoon} from "./cloudy-moon.svg";
-import {ReactComponent as BatMoon} from "./bat-moon.svg";
-import {ReactComponent as Sun} from "./sun.svg";
-import {ReactComponent as BloodKnife} from "./blood-knife.svg";
-import {ReactComponent as Loader} from "./loader.svg";
+import * as Social from "./social";
+import CloudyMoon from "./cloudy-moon.svg?react";
+import BatMoon from "./bat-moon.svg?react";
+import Sun from "./sun.svg?react";
+import BloodKnife from "./blood-knife.svg?react";
+import Loader from "./loader.svg?react";
+import Swatch from "./swatch.svg?react";
+import Home from "./home.svg?react";
+import Language from "./language.svg?react";
+import Error from "./error.svg?react";
+import Check from "./check.svg?react";
 
-export const Icons = {Flags, CloudyMoon, BatMoon, Sun, BloodKnife, Loader};
+export const Icons = {
+    Flags,
+    Social,
+    CloudyMoon,
+    BatMoon,
+    Sun,
+    BloodKnife,
+    Loader,
+    Swatch,
+    Home,
+    Language,
+    Error,
+    Check,
+};
---
+++ apps/web/src/shared/ui/icons/social/index.ts
@@ -0,0 +1,3 @@
+export {default as Google} from "./google.svg?react";
+export {default as Github} from "./github.svg?react";
+export {default as Steam} from "./steam.svg?react";
