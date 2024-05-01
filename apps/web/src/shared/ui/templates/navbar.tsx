594795aca15c76bbc2982b93f53346269a4b1528
Ansat Euler <iffypixy@gmail.com>
2024-05-02 00:26:14 +0200

    refactor: updated imports

--- apps/web/src/shared/ui/templates/navbar.tsx
+++ apps/web/src/shared/ui/templates/navbar.tsx
@@ -1,9 +1,9 @@
 import {Link} from "wouter";
-import {BiHomeAlt} from "react-icons/bi";
 import {cx} from "class-variance-authority";
 
 import {LanguageDropdown} from "@shared/lib/i18n";
-import {ThemeDropdown} from "@shared/lib/theming";
+import {ThemeDropdown} from "@shared/lib/themes";
+import {Icons} from "@shared/ui";
 
 import {Fullscreen} from "./fullscreen";
 
@@ -13,12 +13,12 @@ export const NavbarTemplate: React.FC<NavbarTemplateProps> = (props) => (
     <div
         {...props}
         className={cx(
-            "flex flex-row bg-paper-secondary px-6 py-4 rounded-xl space-x-4 fixed right-[50%] translate-x-1/2 bottom-10 sm:right-2 sm:bottom-2 shadow-even-lg shadow-paper-secondary",
+            "flex flex-row bg-paper-secondary px-6 py-4 rounded-xl space-x-4 fixed right-[50%] translate-x-1/2 bottom-10 sm:right-2 sm:bottom-2 shadow-even-lg shadow-paper-secondary z-10",
             props.className,
         )}
     >
         <Link to="/">
-            <BiHomeAlt className="w-14 h-14 rounded-lg p-2 hover:shadow-even-lg hover:shadow-primary bg-primary text-primary-contrast transition duration-500 cursor-pointer hover:-translate-y-2" />
+            <Icons.Home className="w-14 h-14 rounded-lg p-2 hover:shadow-even-lg hover:shadow-primary bg-primary text-primary-contrast transition duration-500 cursor-pointer hover:-translate-y-2" />
         </Link>
 
         <LanguageDropdown
