bdba6e48b6780d50537f8fb50b105fff67e646d1
Ansat Euler <iffypixy@gmail.com>
2024-05-02 00:24:52 +0200

    feat: added a bunch of icons

--- apps/web/src/shared/ui/icons/index.ts
+++ apps/web/src/shared/ui/icons/index.ts
@@ -1,5 +1,6 @@
 import * as Flags from "./flags";
 import * as Social from "./social";
+import * as Companies from "./companies";
 import CloudyMoon from "./cloudy-moon.svg?react";
 import BatMoon from "./bat-moon.svg?react";
 import Sun from "./sun.svg?react";
@@ -10,10 +11,23 @@ import Home from "./home.svg?react";
 import Language from "./language.svg?react";
 import Error from "./error.svg?react";
 import Check from "./check.svg?react";
+import Start from "./start.svg?react";
+import Handcuffs from "./handcuffs.svg?react";
+import Parking from "./parking.svg?react";
+import Police from "./police.svg?react";
+import Empire from "./empire.svg?react";
+import Chance from "./chance.svg?react";
+import Tax from "./tax.svg?react";
+import RivalTax from "./rival-tax.svg?react";
+import Water from "./water.svg?react";
+import Electricity from "./electricity.svg?react";
+import Prize from "./prize.svg?react";
+import Money from "./money.svg?react";
 
 export const Icons = {
     Flags,
     Social,
+    Companies,
     CloudyMoon,
     BatMoon,
     Sun,
@@ -24,4 +38,16 @@ export const Icons = {
     Language,
     Error,
     Check,
+    Start,
+    Handcuffs,
+    Parking,
+    Police,
+    Empire,
+    Chance,
+    Tax,
+    RivalTax,
+    Water,
+    Electricity,
+    Prize,
+    Money,
 };
