585550f055ec0cf0b7a9f7b1da67b4ac4325eb0f
Ansat Euler <iffypixy@gmail.com>
2024-05-02 00:24:13 +0200

    feat: added nullish type

--- apps/web/src/shared/lib/types/index.ts
+++ apps/web/src/shared/lib/types/index.ts
@@ -1,3 +1,4 @@
 export type Icon = React.FC<React.ComponentProps<"svg">>;
 export type Nullable<T> = T | null;
+export type Nullish<T> = T | null | undefined;
 export type Callback = (...args: any[]) => void;
