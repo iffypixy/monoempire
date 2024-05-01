ab5e5e6259a1df69f89906d1289c3b67daceb531
Ansat Euler <iffypixy@gmail.com>
2024-05-02 00:23:37 +0200

    feat: set withCredentials flag to true

--- apps/web/src/shared/lib/request/index.ts
+++ apps/web/src/shared/lib/request/index.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const request = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
+    withCredentials: true,
 });
 
 export interface Dto<Req, Res> {
